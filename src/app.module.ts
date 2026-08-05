91a81f26264ed5084d241443c74319ff4e4a4a83
Amirhossein <hosseinpc837@gmail.com>
2026-08-05 22:17:45 +0330

    added sms module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { SmsModule } from './sms/sms.module';
+import { ProfileModule } from './user/profile/profile.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    SmsModule,
+    ProfileModule,
   ],
   controllers: [AppController],
   providers: [AppService],
