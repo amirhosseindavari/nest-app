91004b922eefe48dc6a3abe48dc6fd2106976525
Amirhossein <hosseinpc837@gmail.com>
2026-08-14 00:11:26 +0330

    some changes

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // swagger config
+  const config = new DocumentBuilder()
+    .setTitle('Nest Js Application')
+    .setDescription('This is my first nest application')
+    .addBearerAuth()
+    .setVersion('2.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app,config);
+
+  SwaggerModule.setup('api/docs' , app , document);
+
   await app.listen(process.env.PORT ?? 3000);
 
   // api_key
